ca9d4db161b509109430e12c4947b21231f3c955
matthewqu14 <matthewqu@college.harvard.edu>
2021-01-06 01:29:14 -0600

    Added grey line plots to lag/lead regression graphs

--- regression.py
+++ regression.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("covid_data/us_increase.csv")
+state_df = pd.read_csv("covid_data/us_states_increase.csv")
 
 df['date'] = pd.to_datetime(df['date'])
 mask = (df['date'] > '2020-09-29') & (df['date'] <= '2020-12-28')
@@ -11,10 +12,17 @@ df = df.loc[mask]
 df.set_index('date', inplace=True)
 df['cases'] = df['cases'].div(1000)  # cases in thousands
 
+state_mask = (state_df['date'] > '2020-09-29') & (state_df['date'] <= '2020-12-28')
+state_df['date'] = pd.to_datetime(state_df['date'])
+state_df = state_df.loc[state_mask]
+state_df.set_index('date', inplace=True)
+
 keywords = ["loss of taste", "covid symptoms", "loss of smell", "face mask", "coronavirus vaccine", "covid testing"]
+
 for word in keywords:
     df_rsv = pd.read_csv(f"US_google_trends/{word}.csv", index_col="date")
     df = pd.concat([df, df_rsv], axis=1)
+    state_df = pd.concat([state_df, df_rsv], axis=1)
     plt.figure(figsize=(10, 7.5))
     sns.regplot(x=word, y="cases", data=df, color='#C63F3F', scatter_kws={'s': 12})  # ci=None
     plt.ylabel("Increase in cases, thousands")
@@ -41,9 +49,17 @@ def df_shifted(df, target=None, lag=0):
     return pd.DataFrame(data=new)
 
 
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]  # include territories such as PR?
+
+MAX_DAY = 14
+
 for word in keywords:
     points = []
-    for i in range(-14, 15):
+    for i in range(MAX_DAY + 1):  # positive i means RSV from i days earlier is compared with current cases
         new_df = df_shifted(df, 'cases', lag=i)
         points.append((i, new_df.corr().loc['cases', word]))
 
@@ -51,7 +67,14 @@ for word in keywords:
     plt.plot(*zip(*points), color='r')
     plt.ylabel("Correlation")
     plt.xlabel("Lag/Lead")
-    # plt.yticks(np.arange(0.25, 1.01, step=0.25)) perhaps use consistent ticks
+    # plt.yticks(np.arange(0.25, 1.01, step=0.25))  # perhaps use consistent ticks
+    # plt.ylim(0, 1)
     x, y = zip(*points)
-    plt.title(f"{word.title()}, max: {max(y):.3f}")
+    plt.title(f"{word.title()}, max: {max(y):.3f}, argmax: {np.argmax(y)}")
+    for state in states:
+        points = []
+        for i in range(MAX_DAY + 1):
+            new_df = df_shifted(state_df.loc[:, [state, word]], state, lag=i)
+            points.append((i, new_df.corr().loc[state, word]))
+        plt.plot(*zip(*points), color='grey', alpha=0.3)
     plt.show()
