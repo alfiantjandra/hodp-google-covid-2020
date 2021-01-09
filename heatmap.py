ff4f95ee64881aaba1e5a26b176e67c13120f2e3
matthewqu14 <matthewqu@college.harvard.edu>
2021-01-09 16:17:26 -0600

    Created visual for total US cases

--- heatmap.py
+++ heatmap.py
@@ -13,7 +13,7 @@ state_df = pd.DataFrame(data=d)
 fig = px.choropleth(state_df,  # Input pandas DataFrame
                     locations="states",  # DataFrame column with locations
                     color="r-value",  # DataFrame column with values
-                    color_continuous_scale=['#760000', '#F1D3CF'],
+                    color_continuous_scale=['#F1D3CF', '#760000'],
                     # hover_name="states",
                     hover_data={"states": False, "r-value": True},
                     locationmode="USA-states")
